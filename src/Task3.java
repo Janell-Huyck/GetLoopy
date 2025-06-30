44a8d3f3a9804b3c4fe97a603b9bc825584a49fe
Janell-Huyck <huyckjl@ucmail.uc.edu>
2025-06-30 16:37:56 -0400

    Change files to 8 task files

--- src/Task3.java
+++ src/Task3.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class PartC {
+public class Task3 {
     public static void main(String[] args) {
 
     }
--- src/Task8.java
+++ src/Task8.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class PartB {
+public class Task8 {
     public static void main(String[] args) {
 
     }
